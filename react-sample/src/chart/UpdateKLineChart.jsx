2179fcd251d0060d5432d1a56696bcbf0ea79142
luyomo <syunka.tyo@gmail.com>
2021-08-26 12:10:38 +0900

    Replace the data request to websocket

--- react-sample/src/chart/UpdateKLineChart.jsx
+++ react-sample/src/chart/UpdateKLineChart.jsx
@@ -5,29 +5,37 @@ import Layout from '../Layout'
 
 export default function UpdateKLineChart () {
   function updateData (kLineChart) {
-    setTimeout(() => {
-      if (kLineChart) {
-        const dataList = kLineChart.getDataList()
-        const lastData = dataList[dataList.length - 1]
-        const newData = generatedKLineDataList(lastData.timestamp, lastData.close, 1)[0]
-        newData.timestamp += 1000 * 60
-
-        const asyncFetch = () => {
-          fetch('https://www.51yomo.net/stock-api/tickData')
-            .then((response) => response.json())
-            .then((json) => {
-              for (const val of json) {
-                kLineChart.updateData(val)
-              }
-            })
-            .catch((error) => {
-              console.log('fetch data failed', error)
-            })
-        }
-        asyncFetch()
+    if (kLineChart) {
+      var ws = new WebSocket('wss://www.51yomo.net/tickData')
+      ws.onopen = function () {
+        // console.log('About to send data at onmessage')
+        ws.send('Hello World')
+        // console.log('Message sent! at onmessage')
       }
-      updateData(kLineChart)
-    }, 1000)
+
+      ws.onmessage = function (evt) {
+        // console.log('About to receive data at onmessage')
+        var receivedMsg = JSON.parse(evt.data)
+        // console.log('Message received = ' + typeof (receivedMsg) + ' at onmessage')
+        // console.log(receivedMsg)
+        for (const val of receivedMsg) {
+          // console.log('The data is ' + val)
+          kLineChart.updateData(val)
+        }
+      }
+
+      ws.onclose = function (event) {
+        if (event.wasClean) {
+          console.log(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`)
+        } else {
+          console.log('[close] Connection died')
+        }
+      }
+
+      ws.onerror = function (error) {
+        alert(`[error] ${error.message}`)
+      }
+    }
   }
 
   useEffect(() => {
